10bb2abeb16951c607a17d3d18c80967474234e9
Dmitry Cherkashin <dchpost@mail.ru>
2024-12-19 07:17:12 +1000

    добавление хука для фильтрации пользователей

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,30 +1,32 @@
 import { useEffect, useState, useCallback } from 'react'
 import Button from './Button/Button.jsx'
 import Modal from './Modal/Modal.jsx'
+import useInput from '../hooks/useInput.js'
 
 
 export default function EffectSection() {
-const [modal, setModal] = useState(false)
-const [loading, setLoading] = useState(false)
-const [users, setUsers] = useState([])
+    const input = useInput()
+    const [modal, setModal] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
 
-const fetchUsers = useCallback(async () => {
-    setLoading(true)
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await response.json()
-    setUsers(users)
-    setLoading(false)
-}, [])
+    const fetchUsers = useCallback(async () => {
+        setLoading(true)
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        setUsers(users)
+        setLoading(false)
+    }, [])
 
-useEffect(() => {
-    fetchUsers()
-}, [fetchUsers])
+    useEffect(() => {
+        fetchUsers()
+    }, [fetchUsers])
 
     return (
         <section>
             <h3>Effects</h3>
 
-            <Button style={{marginBottom: '1rem'}} onClick={() => setModal(true)}>Открыть информацию</Button>
+            <Button style={{ marginBottom: '1rem' }} onClick={() => setModal(true)}>Открыть информацию</Button>
 
             <Modal open={modal}>
                 <h3>Hello from modal</h3>
@@ -34,9 +36,21 @@ useEffect(() => {
 
             {loading && <p>Loading...</p>}
 
-            {!loading && <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-                </ul>}
+            {!loading && (
+                <>
+                    <input type='text' className='control' 
+                    {...input}
+                    />
+                    {/* <h6>{input.value}</h6> */}
+                    <ul>
+                        {users
+                        .filter(user => 
+                            user.name.toLowerCase().includes(input.value.toLowerCase()))
+                            .map((user => <li key={user.id}>{user.name}</li>
+                        ))}
+                    </ul>
+                </>
+            )}
         </section>
     )
 }
